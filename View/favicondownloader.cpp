ccc11ebf7717e91aa427c4cbd0ed4c722417539e
Edward Heretsch <edward.heretsch@protonmail.com>
2021-03-19 18:58:24 +0100

    Feature: favicon: Further work to download the favicon

--- View/favicondownloader.cpp
+++ View/favicondownloader.cpp
@@ -11,10 +11,12 @@ FaviconDownloader::FaviconDownloader(QString iconPath, QString urlPath)
 {
     this->iconPath = iconPath;
     QUrl url = QUrl(urlPath);
-    QString faviconUrl = url.toString(QUrl::RemovePath) + "/favicon.ico";
+    QString faviconUrl = url.toString(QUrl::RemovePath | QUrl::RemoveScheme) + "/favicon.ico";
+    faviconUrl = "http:" + faviconUrl;
     qDebug() << faviconUrl;
     QNetworkAccessManager *mgr = new QNetworkAccessManager(this);
-    QNetworkRequest request(url);
+    QUrl furl(faviconUrl);
+    QNetworkRequest request(furl);
     connect(mgr, SIGNAL(finished(QNetworkReply*)), this, SLOT(on_queryFinish(QNetworkReply*)));
     connect(mgr, SIGNAL(finished(QNetworkReply*)), mgr, SLOT(deleteLater()));
     const char* mgrSslSignal = SIGNAL(sslErrors(QNetworkReply* reply, const QList<QSslError>& errors));
@@ -36,12 +38,15 @@ void FaviconDownloader::on_deleteLater()
 
 void FaviconDownloader::on_queryFinish(QNetworkReply *reply)
 {
+    qDebug() << "error: " << reply->error();
+    qDebug() << "error string: " << reply->errorString();
+    qDebug() << "finished: " << reply->isFinished();
     QFileInfo fileInfo(iconPath);
     QString iconFile = QDir(fileInfo.absolutePath()).filePath(fileInfo.completeBaseName()) + ".ico";
     QFile file(iconFile);
     file.open(QIODevice::WriteOnly);
     file.write(reply->readAll());
-    delete reply;
+    file.close();
 }
 
 void FaviconDownloader::on_SslErrors(QNetworkReply* reply, const QList<QSslError>& errors)
