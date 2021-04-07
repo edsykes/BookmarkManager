dad9e8c27b54a25193686f6e145cee9190bb9f47
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-07 21:44:23 +0200

    Build: changing file references to include a dot

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -3,9 +3,9 @@
 make.exe clean -j400
 pwd
 ls
-qmake BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
+qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 ls
-make.exe -f Makefile qmake_all
+make.exe -f ./Makefile qmake_all
 echo starting the final build
 make.exe -j4
 
