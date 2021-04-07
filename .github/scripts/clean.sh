2c5511aaac161b05eaac8e04779ac432d0694a73
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-07 22:14:05 +0200

    Build: Added logging of the make file env var

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -10,6 +10,7 @@ ls
 qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 ls
 make.exe -f ./Makefile qmake_all
+echo make command is $(MAKE)
 echo starting the final build
 cd D:/a/BookmarkManager/BookmarkManager
 cat Makefile
