404354eed9bfca2efdcf4b22c4bf9fe9cfe187d2
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-07 21:52:58 +0200

    Build: adding an explicit move to the directory that seems to have trouble

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -7,5 +7,6 @@ qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 ls
 make.exe -f ./Makefile qmake_all
 echo starting the final build
+cd D:/a/BookmarkManager/BookmarkManager
 make.exe -j4
 
