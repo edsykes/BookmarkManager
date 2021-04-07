e44d573ee2d54846504b62fe8171284f17ece2b8
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-07 22:21:25 +0200

    Build: Extending logging of the env vars

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -10,7 +10,7 @@ ls
 qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 ls
 make.exe -f ./Makefile qmake_all
-echo make command is $(MAKE)
+echo make command is $(MAKE) -f $(MAKEFILE).Debug
 echo starting the final build
 cd D:/a/BookmarkManager/BookmarkManager
 cat Makefile
