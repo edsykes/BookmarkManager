fffc26b7d8bc65f3f28c58547fc77a1da8ab49cf
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-08 07:54:00 +0200

    Build: Added echo for tracing

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 make.exe clean -j400
+echo print current directory
 pwd
+echo go to parent
 cd ..
+echo print parent
 pwd
 ls
+echo go to BookmarkManager and print
 cd BookmarkManager
 ls
+echo start qmake
 qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
+echo list files post qmake
 ls
+echo create make file
 make.exe -f ./Makefile qmake_all
-echo make command is $(MAKE) -f $(MAKEFILE).Debug
+echo make command is $(MAKE) 
 echo starting the final build
 cd D:/a/BookmarkManager/BookmarkManager
-cat Makefile
+echo start make
 make.exe -j4
 
