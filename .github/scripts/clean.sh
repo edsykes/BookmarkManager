639a7a3d016d1719ca3f28d0d195e36195720e63
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-09 07:38:46 +0200

    Build: Corrected qt to Qt

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -16,7 +16,7 @@ qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 echo list files post qmake
 ls
 echo qt library dir listing
-ls qt/5.15.2/mingw81_64/lib/
+ls Qt/5.15.2/mingw81_64/lib/
 echo create make file
 make.exe -n -f ./Makefile qmake_all
 echo make command is $(MAKE) 
