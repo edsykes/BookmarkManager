66553b4e75f19efb68bc81e4d1044d94683e1e9f
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-09 07:47:33 +0200

    Build: Adding listings of path to libs

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -16,6 +16,9 @@ qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 echo list files post qmake
 ls
 echo qt library dir listing
+ls ../Qt
+ls ../Qt/5.15.2/
+ls ../Qt/5.15.2/mingw81_64
 ls ../Qt/5.15.2/mingw81_64/lib/
 echo create make file
 make.exe -n -f ./Makefile qmake_all
