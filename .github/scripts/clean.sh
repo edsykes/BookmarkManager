e643981ea5e06b86cebc46f4d68f35eabf565644
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-10 19:50:32 +0200

    Build: removed the -n of the make to move to a real build

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -4,7 +4,7 @@ make.exe clean -j400
 echo start qmake
 qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=release" "CONFIG+=qml_release"
 echo create make file
-make.exe -n -j4
+make.exe -j4
 ls
 cd debug
 echo debug dir contents
