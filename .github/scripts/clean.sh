7a754ee65e568c21c4c27f9c3b6a57031f49fa31
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-10 16:13:27 +0200

    Build: simplified the build script

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -1,37 +1,8 @@
 #!/bin/bash
 
 make.exe clean -j400
-echo print current directory
-pwd
-echo go to parent
-cd ..
-echo print parent
-pwd
-ls
-echo go to BookmarkManager and print
-cd BookmarkManager
-ls
 echo start qmake
 qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
-echo list files post qmake
-ls
-echo qt library dir listing
-ls ../Qt
-ls ../Qt/5.15.2/
-ls ../Qt/5.15.2
-ls ../Qt/5.15.2/msvc2019_64
-ls ../Qt/5.15.2/msvc2019_64/lib/
 echo create make file
-make.exe -n -f ./Makefile qmake_all
-echo make command is $(MAKE) 
-echo starting the final build
-cd D:/a/BookmarkManager/BookmarkManager
-echo cat the debug make file
-cat Makefile.Debug
-pwd
-ls
-echo start make
-C:/ProgramData/Chocolatey/lib/mingw/tools/install/mingw64/bin/mingw32-make -n -f Makefile.Debug
-
-#make.exe -n -j4
+make.exe -n -j4
 
