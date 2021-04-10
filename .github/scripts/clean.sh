98c8752dc0d05be3a8a2d3531fb5471178bae4d6
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-10 16:27:33 +0200

    Build: Chnaged build to release

--- .github/scripts/clean.sh
+++ .github/scripts/clean.sh
@@ -2,8 +2,13 @@
 
 make.exe clean -j400
 echo start qmake
-qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
+qmake ./BookmarkManager.pro -spec win32-g++ "CONFIG+=release" "CONFIG+=qml_release"
 echo create make file
 make.exe -n -j4
 ls
-
+cd debug
+echo debug dir contents
+ls
+cd ../release
+echo release dir contents
+ls
