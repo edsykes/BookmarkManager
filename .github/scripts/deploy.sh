9f1c5f6e8ad31ff5e7ff904f1be2279d64c380f9
Edward Heretsch <edward.heretsch@protonmail.com>
2021-04-14 18:24:46 +0200

    Build: Removed passing the build number to the deploy script as it's not needed

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir deployment-$1
+mkdir deployment
 echo $BUILD_NUMBER
-windeployqt release/BookmarkManager.exe --dir deployment-$1
-cp release/BookmarkManager.exe deployment-$1
+windeployqt release/BookmarkManager.exe --dir deployment
+cp release/BookmarkManager.exe deployment
